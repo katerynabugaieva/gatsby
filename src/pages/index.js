598299881d6aac1b0fb7ed00c22f0b17cd8925c4
katerynabugaieva <sonnensmile@gmail.com>
2020-08-04 14:49:16 +0200

    buikding blocks

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react"
+import Header from '../components/header'
+import { Link } from 'gatsby'
 
 export default function Home() {
-  return <div>gatsby starts</div>
+  return (
+  <div style={{ color: `purple` }}>
+    <Link to="/contact/">Contact</Link>
+    <Header headerText="Run,Gatsby"/>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+  </div>
+  )
 }
